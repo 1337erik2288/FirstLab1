3a5d3ce865771158e912f930e7aed8efeb172c19
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2023-02-15 17:47:25 +0800

    delete text cherez button

--- src/SimpleFrame.java
+++ src/SimpleFrame.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,6 +11,21 @@ public class SimpleFrame extends Frame {
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent ev){
                 System.exit(0);
+            }
+        });
+        setLayout(null);
+        Label text = new Label("Stroka");
+        text.setLocation(50, 50);
+        text.setSize(150, 20);
+        add(text);
+        Button del = new Button("Delete text");
+        del.setLocation(50, 100);
+        del.setSize(100, 20);
+        add(del);
+        del.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                text.setText("");
             }
         });
         setVisible(true);
